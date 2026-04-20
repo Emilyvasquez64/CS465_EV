8741177f281e992fec9fbafcfe3d2f6d636f9b12
Emily Vasquez <Girlfriend@Evans-MBP.attlocal.net>
2026-04-19 19:32:00 -0700

    Module 7 completed baseline

--- app_admin/src/app/trip-listing/trip-listing.ts
+++ app_admin/src/app/trip-listing/trip-listing.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { TripDataService } from '../services/trip-data';
 import { TripCard } from '../trip-card/trip-card';
 import { Trip } from '../models/trip';
+import { AuthenticationService } from '../services/authentication';
 
 @Component({
   selector: 'app-trip-listing',
@@ -15,26 +16,25 @@ import { Trip } from '../models/trip';
 export class TripListing implements OnInit {
   trips: Trip[] = [];
   message: string = '';
-
   constructor(
     private tripDataService: TripDataService,
     private router: Router,
-    private cdr: ChangeDetectorRef
+    private cdr: ChangeDetectorRef,
+    private authenticationService: AuthenticationService
   ) {}
-
   ngOnInit(): void {
-    console.log('ngOnInit called');
     this.getTrips();
   }
 
+  public isLoggedIn(): boolean {
+    return this.authenticationService.isLoggedIn();
+  }
+
   public getTrips(): void {
-    console.log('getTrips called');
     this.tripDataService.getTrips().subscribe({
       next: (value: any) => {
-        console.log('trips received:', value);
         this.trips = [...value];
         this.message = `There are ${this.trips.length} trips available.`;
-        console.log(this.message);
         this.cdr.detectChanges();
       },
       error: (error: any) => {
@@ -42,7 +42,6 @@ export class TripListing implements OnInit {
       }
     });
   }
-
   public addTrip(): void {
     this.router.navigate(['add-trip']);
   }
